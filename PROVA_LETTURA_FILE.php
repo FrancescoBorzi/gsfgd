80282cb57746ff273d54b00452572f5b7c58771b
Sal Cal <dio_cane@rocketmail.com>
2012-05-31 16:08:20 +0200

    prova lettura testo

--- PROVA_LETTURA_FILE.php
+++ PROVA_LETTURA_FILE.php
@@ -9,13 +9,12 @@
         echo "dio\n";
              
         $par="DPD_Drug_ID_Number";
-        $testo=file("./files/DrugBank/drugbank.txt");
+        $testo=file("./drugbank.txt");
         reset($testo);
         $linea=1;
         $rigasuccessiva=1;
-        $limiteRicerca=2000;
-        $percorso="/home/fortunso/Scrivania/out.txt";
-        //$write_file = fopen("./out.txt","w");
+       
+        //$write_file = fopen("./out.txt","w+");
         while(list(,$value)= each($testo)) {
         
             $str=strpos($value,$par);
@@ -31,7 +30,7 @@
     
             if(($rigasuccessiva+1)==($linea)){
                 //fwrite($write_file,"$value\n");
-                print_r("$value\n");
+                //print_r("$value\n");
                 //print_r("$linea\n");
                
                
@@ -39,35 +38,31 @@
             $linea++;
                 
         }
-                  //fclose($write_file);   
+                  fclose($write_file);   
         
         //FUNZIONE NON UTILIZZATA
-        function readLine($file, $line_num, $delimiter="\n")
-{
-    /*** set the counter to one ***/
-    $i = 1;
- 
-    /*** open the file for reading ***/
-    $fp = fopen( $file, 'r' );
- 
-    /*** loop over the file pointer ***/
-    while ( !feof ( $fp) )
-    {
-        /*** read the line into a buffer ***/
-        $buffer = stream_get_line( $fp, 1024, $delimiter );
-        /*** if we are at the right line number ***/
-        if( $i == $line_num )
-        {
-            /*** return the line that is currently in the buffer ***/
-            return $buffer;
+        function readLine($file, $line_num, $delimiter="\n"){
+
+            /***contatore ***/
+            $i = 1;
+            /*** apri il file da leggere ***/
+            $fp = fopen( $file, 'r' );
+            /*** cicla fino la fine del file ***/
+            while ( !feof ( $fp) )
+            {
+                /*** leggi la riga ***/
+                $buffer = stream_get_line( $fp, 1024, $delimiter );
+                /*** se siamo nella giusta linea***/
+                if( $i == $line_num )
+                {
+                    /*** ritorna la linea attuale del buffer ***/
+                    return $buffer;
+                }
+                $i++;
+                $buffer = '';
+            }
+            return false;
         }
-        /*** increment the line counter ***/
-        $i++;
-        /*** clear the buffer ***/
-        $buffer = '';
-    }
-    return false;
-}
         ?>
     </body>
 </html>
