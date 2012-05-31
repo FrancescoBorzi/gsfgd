f8ac9f3c56dc27e13f05c8e9f845d7ae67854ac0
ShinDarth <shin.darth@hotmail.it>
2012-05-31 17:29:14 +0200

    - Aggiunto un esempio di singola drug nella cartella /doc/ - (ANCORA DA FINIRE) tabella per le drug nella cartella /sql/

---
+++ sql/drugbank_table.sql
@@ -0,0 +1,19 @@
+# INCOMPLETO
+
+CREATE TABLE drugbank (
+	id_drugbank VARCHAR(7) NOT NULL,
+	name VARCHAR(30),
+	description VARCHAR(255),
+	toxixity VARCHAR(255),
+	atc_code VARCHAR(7),
+	action VARCHAR(20),
+	pharmacology VARCHAR(255),
+	formula_molecolare VARCHAR(20),
+	drug-interaction
+	categories
+	target-position
+	target-partner
+	property
+	
+	PRIMARY KEY (id_drugbank),
+)
