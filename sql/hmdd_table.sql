362fe0e608a128f48ed3f8c900dd4648c501342b
ShinDarth <shin.darth@hotmail.it>
2012-10-05 18:10:52 +0200

    updated hmdd table struct

--- sql/hmdd_table.sql
+++ sql/hmdd_table.sql
@@ -1,22 +1,8 @@
--- DA COMPLETARE
-
-DROP TABLE IF EXISTS hmdd;
-CREATE TABLE hmdd (
-	mirna VARCHAR(20) NOT NULL,
-	conservation_group SMALLINT,
-	PRIMARY KEY (mirna)
-);
-
-DROP TABLE IF EXISTS hmdd_function;
-CREATE TABLE hmdd_function (
-	mirna VARCHAR(20) NOT NULL,
-	function VARCHAR(50) NOT NULL,
-	PRIMARY KEY (mirna, function)
-);
-
 DROP TABLE IF EXISTS hmdd_disease;
 CREATE TABLE hmdd_disease (
 	mirna VARCHAR(20) NOT NULL,
 	disease VARCHAR(50) NOT NULL,
 	PRIMARY KEY(mirna, disease)
 );
+
+
