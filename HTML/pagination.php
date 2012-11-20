c47cd3b730c6fcd0f0452694b1423380e80755c1
fortunso <fortunso@fortunso88>
2012-11-20 23:56:57 +0100

    giga suca

--- HTML/pagination.php
+++ HTML/pagination.php
@@ -1,42 +1,43 @@
 <?php
+$prima=1;
 
-$paginazione = "Pagine totali: " . $tot_pages . "
+$paginazione = "Pagine totali:   " . $tot_pages . "  
 [";
+
+$paginazione.="<a href=\"?page=$prima\" title=\"Go tu first page\"> << </a> ";
 
 $current=0;
 if (isset($_GET["page"])) { $current =$_GET["page"]; } else { $current=1; }; 
 
-if($current>=6){
+if($current>=6 && $current<$tot_pages-6){
     for($i = $current-5; $i <= $current+5; $i++) {
         if($i == $current_page) {
             $paginazione .= $i . " ";
         } else {
-             $paginazione .= "<a href=\"?page=$i\" title=\"Vai alla pagina $i\">$i</a> ";
+             $paginazione .= "<a href=\"?page=$i\" title=\"Go to page $i\">$i</a> ";
         }
     }
-    $paginazione .= "]";
 }
 if($current>=1 && $current<6){
     for($i = 1; $i <= 10; $i++) {
         if($i == $current_page) {
             $paginazione .= $i . " ";
         } else {
-             $paginazione .= "<a href=\"?page=$i\" title=\"Vai alla pagina $i\">$i</a> ";
+             $paginazione .= "<a href=\"?page=$i\" title=\"Go to page $i\">$i</a> ";
         }
     }
-    $paginazione .= "]";
 }
-if($current>=$tot_pages-5){
-    for($i = $current-5; $i <= $current+5; $i++) {
+if($current>=$tot_pages-6){
+    for($i = $current-8; $i <= $tot_pages; $i++) {
         if($i == $current_page) {
             $paginazione .= $i . " ";
         } else {
-             $paginazione .= "<a href=\"?page=$i\" title=\"Vai alla pagina $i\">$i</a> ";
+             $paginazione .= "<a href=\"?page=$i\" title=\"Go to page $i\">$i</a> ";
         }
     }
-    $paginazione .= "]";
 }
 
-
+$paginazione.="<a href=\"?page=$tot_pages\" title=\"Go to last page\"> >> </a> ";
+$paginazione .= "]";
 
 ?> 
