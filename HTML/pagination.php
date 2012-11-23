d438f301f01167da238683bba7a24a1f65af876d
fortunso <fortunso@fortunso88>
2012-11-23 17:49:37 +0100

    Fix scorrimento pagine

--- HTML/pagination.php
+++ HTML/pagination.php
@@ -9,34 +9,45 @@ $paginazione.="<a href=\"?page=$prima&testo=$query\" title=\"Go tu first page\">
 $current=0;
 if (isset($_GET["page"])) { $current =$_GET["page"]; } else { $current=1; }; 
 
-if($current>=6 && $current<$tot_pages-6){
-    for($i = $current-5; $i <= $current+5; $i++) {
+if ($tot_pages<=10) {
+    for($i = 1; $i <= $tot_pages; $i++) {
         if($i == $current_page) {
             $paginazione .= $i . " ";
         } else {
              $paginazione .= "<a href=\"?page=$i&testo=$query\" title=\"Go to page $i\">$i</a> ";
         }
     }
-}
-if($current>=1 && $current<6){
-    for($i = 1; $i <= 10; $i++) {
-        if($i == $current_page) {
-            $paginazione .= $i . " ";
-        } else {
-             $paginazione .= "<a href=\"?page=$i&testo=$query\" title=\"Go to page $i\">$i</a> ";
-        }
-    }
-}
-if($current>=$tot_pages-6){
-    for($i = $current-8; $i <= $tot_pages; $i++) {
-        if($i == $current_page) {
-            $paginazione .= $i . " ";
-        } else {
-             $paginazione .= "<a href=\"?page=$i&testo=$query\" title=\"Go to page $i\">$i</a> ";
-        }
-    }
-}
 
+}
+else {
+	if($current>=6 && $current<$tot_pages-6){
+	    for($i = $current-5; $i <= $current+5; $i++) {
+		if($i == $current_page) {
+		    $paginazione .= $i . " ";
+		} else {
+		     $paginazione .= "<a href=\"?page=$i&testo=$query\" title=\"Go to page $i\">$i</a> ";
+		}
+	    }
+	}
+	if($current>=1 && $current<6){
+	    for($i = 1; $i <= 10; $i++) {
+		if($i == $current_page) {
+		    $paginazione .= $i . " ";
+		} else {
+		     $paginazione .= "<a href=\"?page=$i&testo=$query\" title=\"Go to page $i\">$i</a> ";
+		}
+	    }
+	}
+	if($current>=$tot_pages-6){
+	    for($i = $current-8; $i <= $tot_pages; $i++) {
+		if($i == $current_page) {
+		    $paginazione .= $i . " ";
+		} else {
+		     $paginazione .= "<a href=\"?page=$i&testo=$query\" title=\"Go to page $i\">$i</a> ";
+		}
+	    }
+	}
+}
 $paginazione.="<a href=\"?page=$tot_pages&testo=$query\" title=\"Go to last page\"> >> </a> ";
 $paginazione .= "]";
 
