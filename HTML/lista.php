1ea132ae80bbef483fe1a981396986bebefcafe1
fortunso <fortunso@fortunso88>
2012-11-27 22:13:17 +0100

    giga suca, fix vari

--- HTML/lista.php
+++ HTML/lista.php
@@ -131,7 +131,7 @@
 	// esecuzione seconda query con LIMIT
 	$query_limit = mysql_query($query." LIMIT $primo, $per_page");
 	  
-        echo "<div style=\"margin-left: 10px; margin-right: 33390px;width: 100%; overflow: auto;\">
+        echo "<div style=\"margin-left: 60px; margin-right: 90px;\">
              <table id=\"drugs\" class=\"standard\">
               <tr>";
         
