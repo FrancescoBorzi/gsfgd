af3e16e026ec694c7904a68c1fbcf3c472ac6979
fortunso <fortunso@fortunso88>
2012-11-29 17:54:51 +0100

    PERFEZIONAMENTo

--- HTML/search.php
+++ HTML/search.php
@@ -2,8 +2,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        
-
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />	  	
+        <title>gsfgd's Project - Home</title>	  	
+        <meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="styles.css" rel="stylesheet" type="text/css" />
+	<link href="styleTable.css" media="screen" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="js/jquery-ui.css" />
+	<script src="js/jquery-1.8.3.js"></script>
+	<script src="js/jquery-ui.js"></script> 
     </head>
     <body  style="background-image: url(images/b.jpg);">
         <div id="main">
