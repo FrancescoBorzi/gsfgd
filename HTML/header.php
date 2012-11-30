d6b608364e01a4c1317b8b6b3a80bcc23baf454b
Helias <stefano520@hotmail.it>
2012-11-30 18:20:54 +0100

    Modificata distanza dei bottoni del menu, accorciato il codice di un casino ;)

--- HTML/header.php
+++ HTML/header.php
@@ -5,32 +5,8 @@
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
 <div id="header">
        <div id="logo"></div>
-	<style type="text/css">
-	.menu
-	{
-		-webkit-transition: all 0.5s ease-in-out;
-		-moz-transition: all 0.5s ease-in-out;
-		-o-transition: all 0.5s ease-in-out;
-		-ms-transition: all 0.5s ease-in-out;
-		transition: all 0.5s ease-in-out;
-	}
-	.menu:hover
-	{
-		-webkit-transition: all 0.5s ease-in-out;
-		-moz-transition: all 0.5s ease-in-out;
-		-o-transition: all 0.5s ease-in-out;
-		-ms-transition: all 0.5s ease-in-out;
-		transition: all 0.5s ease-in-out;
-		-webkit-transform:scale(2);
-		-moz-transform:scale(2);
-		-o-transform:scale(2);
-		-ms-transform:scale(2)
-	}
-	</style>
-       <div id="buttons">
-		<a class="menu" href="index.php" title="" >Home </a>
-		<a class="menu"  href="search.php" title="">Search</a>
-		<a class="menu" href="list.php" title="">List</a>
-   		<a class="menu" href="query.php" title="">Query</a>
-	</div>
+	<a href="index.php" title="" >Home </a>
+	<a  href="search.php" title="">Search</a>
+	<a href="list.php" title="">List</a>
+	<a href="query.php" title="">Query</a>
 </div>
